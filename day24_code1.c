6ce304e709f9305564c06b1c7d6e318647487d45
SakshiBhandari1008 <Sakshi.36499@stu.upes.ac.in>
2026-09-02 08:24:25 +0530

    Add files via upload

---
+++ day24_code1.c
@@ -0,0 +1,24 @@
+// WAP to print the following:
+// *
+// **
+// *** 
+// ****
+// *****
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
