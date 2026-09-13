cb492d7d433639b2a113ddc51bc7f06a861e4cff
SakshiBhandari1008 <Sakshi.36499@stu.upes.ac.in>
2026-09-13 16:27:56 +0530

    Add files via upload

---
+++ day 35_code1.c
@@ -0,0 +1,30 @@
+//Find the second largest element in an array.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    int i;
+    for ( i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0];
+    int secondLargest = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    printf("%d", secondLargest);
+
+    return 0;
+}
