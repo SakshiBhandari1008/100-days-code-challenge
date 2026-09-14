122f816efb467ff11184b3625a8951111821ccde
SakshiBhandari1008 <Sakshi.36499@stu.upes.ac.in>
2026-09-14 08:27:10 +0530

    Add files via upload

---
+++ day 36_code1.c
@@ -0,0 +1,33 @@
+//Read and print a matrix
+
+#include <stdio.h>
+
+int main()
+{
+    int matrix[10][10];
+    int rows, cols, i, j;
+
+    // Read number of rows and columns
+    scanf("%d %d", &rows, &cols);
+
+    // Read matrix elements
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print the matrix
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
