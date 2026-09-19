2592cae7b9b19d7adff9deda9a6c92a498f09a13
SakshiBhandari1008 <Sakshi.36499@stu.upes.ac.in>
2026-09-19 19:39:26 +0530

    Add files via upload

---
+++ day 41 code 1.c
@@ -0,0 +1,22 @@
+//count characters in a string without using a built-in length functions. 
+
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[count] != '\0')
+    {
+        count++;
+    }
+
+    printf("Number of characters = %d", count);
+
+    return 0;
+}
